9345fd3712ee469def2356cdfb6fbdc317d81238
cfsanderson <calebsanderson@gmail.com>
2017-08-24 22:12:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simon Game
+
+This game is based on @arecvlohe's [simon-workshop](https://github.com/arecvlohe/simon-workshop) on building a vanilla JS Simon game.
---
+++ scripts.js
@@ -0,0 +1,116 @@
+const colors = [...document.getElementsByClassName("box")];
+
+colors.forEach(button => {
+  button.addEventListener("click", () => {
+    const color = button.style.backgroundColor;
+    state.userClicks.push(i)
+    button.style.filter = "opacity(50%)";
+    setTimeout(() => {
+        button.style.filter = "opacity(100%)";
+        if (checkUserInput(sequence, state.userClicks) && equalLengths(state)) {
+
+        }
+    }, 300);
+  })
+})
+
+function checkUserINput(sequence, userClicks) {
+  return userClicks.every(v => {
+    return sequence[v] === v;
+  })
+}
+
+function equalLengths(state, userClicks) {
+  return state.count === userClicks.length;
+}
+
+function playSequence(state) {
+  for (let i = 0; i < state.count; i++) {
+    setTimeout(() => {
+      colors[sequence[0]].style.filter - "opacity(50%)";
+    }, 1000 * (i + 1) + 1000)
+    setTimeout(() => {
+      colors[sequence[0]].style.filter - "opacity(100%)";
+    }, 1300 * (i + 1) + 1000)
+  }
+}
+
+const sequence = Array.from({ length: 20 }, (v, k) => {
+  return Math.floor(Math.random() * 4);
+});
+
+const start = document.getElementsByTagName("button")[0]
+
+const state = {
+  count: 0,
+  userClicks: []
+}
+
+start.addEventListener("click", () => {
+  state.count = 1;
+  colors[sequence[0]].style.filter = "saturate(50%)";
+  setTimeout(() => {
+    colors[sequence[0]].style.filter = "saturate(100%)";
+  }, 300);
+})
+
+
+// const colors = [...document.getElementsByClassName("box")];
+//
+// colors.forEach((button, i) => {
+//   button.addEventListener("click", () => {
+//     const color = button.style.backgroundColor;
+//     state.userClicks.push(i);
+//     button.style.filter = "saturate(50%)";
+//     setTimeout(() => {
+//       button.style.filter = "saturate(100%)";
+//       if (checkUserInput(sequence, state.userClicks) && equalLengths(state)) {
+//         ++state.count;
+//         state.userClicks = [];
+//         playSequence(state);
+//       } else {
+//         playSequence(state);
+//       }
+//     }, 300);
+//   });
+// });
+//
+// function checkUserInput(sequence, userClicks) {
+//   return userClicks.every(v => {
+//     return sequence[v] === v;
+//   });
+// }
+//
+// function equalLengths(state) {
+//   return state.count === state.userClicks.length;
+// }
+//
+// function playSequence(state) {
+//   for (let i = 0; i < state.count; i++) {
+//     setTimeout(() => {
+//       colors[sequence[i]].style.filter = "saturate(50%)";
+//     }, 1000 * (i + 1));
+//     setTimeout(() => {
+//       colors[sequence[i]].style.filter = "saturate(100%)";
+//     }, 1300 * (i + 1));
+//   }
+// }
+//
+// const sequence = Array.from({ length: 20 }, (v, k) => {
+//   return Math.floor(Math.random() * 4);
+// });
+//
+// const start = document.getElementsByTagName("button")[0];
+//
+// const state = {
+//   count: 0,
+//   userClicks: []
+// };
+//
+// start.addEventListener("click", () => {
+//   state.count = 1;
+//   colors[sequence[0]].style.filter = "saturate(50%)";
+//   setTimeout(() => {
+//     colors[sequence[0]].style.filter = "saturate(100%)";
+//   }, 300);
+// });
